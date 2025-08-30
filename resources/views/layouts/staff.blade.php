3fd90f5f7a5c3c709059974348941e59c4b8c791
Ezekiel Muki <ezekielmuki42@gmail.com>
2025-08-30 18:51:59 +0300

    first commit

--- resources/views/layouts/staff.blade.php
+++ resources/views/layouts/staff.blade.php
@@ -6,21 +6,15 @@
                     <x-nav-link :href="route('staff.leads')" :active="request()->routeIs('staff.leads')">
                         {{ __('Leads') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('staff.notes')" :active="request()->routeIs('staff.notes')">
-                        {{ __('Client Notes') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('staff.reminders')" :active="request()->routeIs('staff.reminders')">
-                        {{ __('Reminders') }}
-                    </x-nav-link>
+              
+              
                     <x-nav-link :href="route('staff.commissions')" :active="request()->routeIs('staff.commissions')">
                         {{ __('Commissions') }}
                     </x-nav-link>
                     <x-nav-link :href="route('staff.reports')" :active="request()->routeIs('staff.reports')">
                         {{ __('Reports') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('staff.conversions')" :active="request()->routeIs('staff.conversions')">
-                        {{ __('Conversions') }}
-                    </x-nav-link>
+                  
                     <x-nav-link :href="route('staff.payments')" :active="request()->routeIs('staff.payments')">
                         {{ __('Payment History') }}
                     </x-nav-link>
