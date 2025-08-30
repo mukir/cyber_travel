dd827398a442802b8d686f58511377a701a68cce
Ezekiel Muki <ezekielmuki42@gmail.com>
2025-08-30 18:54:23 +0300

    first commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                      <img src="{{ asset('images/logo.png') }}" style="width: 200px;">
+                      <img src="{{ asset('images/logo.png') }}" style="width: 150px;">
                     </a>
                 </div>
 
