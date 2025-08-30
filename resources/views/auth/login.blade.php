0b51afe60fb228c92c5c6c66b0b20d043aec7f7e
Ezekiel Muki <ezekielmuki42@gmail.com>
2025-08-30 18:58:45 +0300

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,6 +39,13 @@
                 </a>
             @endif
 
+            <!-- Register Link -->
+            <div class="mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Don\'t have an account? Register') }}
+                </a>
+            </div>
+
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
