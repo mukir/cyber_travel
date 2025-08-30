0e5bcfdc74f34d8b61a8799ef00af56df552b7a6
Awasam <emuktech@gmail.com>
2025-08-30 07:41:34 +0300

    Enable ES modules

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+import './style.css';
+
 const app = document.getElementById('app');
 if (app) {
-  app.innerHTML = '<h1>Welcome to Cyber Travel</h1>';
+  app.innerHTML = `<h1 class="text-3xl font-bold text-center text-emerald-700">Welcome to Cyber Travel</h1>`;
 }
